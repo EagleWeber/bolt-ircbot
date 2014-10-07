7f1f3c05ca9c2868ca75945e3537da1a222b2737
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-07 21:43:54 +0100

    Whiskey drinkers step up please
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

--- boltircbot.go
+++ boltircbot.go
@@ -134,6 +134,7 @@ func main() {
 	AddAction(ircproj, `#tea`,  "has boiled some water, and begins to brew %v a nice cup of tea.")
 	AddAction(ircproj, `#wine`,  "opens a bottle of Château Lafite at %v's request!")
 	AddAction(ircproj, `#tequila`,  "drinks one Tequila, two Tequilas, three Tequilas... floor!")
+	AddAction(ircproj, `#whiskey`,  "pours a nip of Glenavon Special for %v's.")
 
 	ircproj.Loop()
 }
