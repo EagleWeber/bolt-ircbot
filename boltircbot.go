d21fe6670329b6062a1e0cfaa2717866b964a042
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-07 22:12:43 +0100

    Regex fail...  Big time!
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

--- boltircbot.go
+++ boltircbot.go
@@ -126,7 +126,7 @@ func main() {
 		}
 	})
 
-	AddAction(ircproj, `#[kitten|cat]`,  "starts to meow at %v... *purr* *purr*")
+	AddAction(ircproj, `#(kitten|cat)`,  "starts to meow at %v... *purr* *purr*")
 	AddAction(ircproj, `#dog`,  "rolls over, and now wants it's tummy scratched by %v")
 	AddAction(ircproj, `#beer`,  "return $this->app['beer']->serve('everyone')->sendBillTo('%v');")
 	AddAction(ircproj, `#coffee`,  "turns on the espresso machine for %v")
