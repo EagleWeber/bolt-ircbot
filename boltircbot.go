7e954c1f604fed9fe9200a1f1a447ce76c456cc3
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-09 10:59:24 +0100

    Address handle
    Signed-off-by: Gawain Lynch <gawain.lynch@gmail.com>

--- boltircbot.go
+++ boltircbot.go
@@ -148,7 +148,7 @@ func main() {
 	AddAction(ircproj, `#wine`,  "opens a bottle of Château Lafite at %v's request!")
 	AddAction(ircproj, `#tequila`,  "drinks one Tequila, two Tequilas, three Tequilas... floor!")
 	AddAction(ircproj, `#whisky`,  "pours a nip of Glenavon Special for %v.")
-	AddAction(ircproj, `#whiskey`,  "takes a swig of Jameson and sings - 'Whack fol de daddy-o, There's whiskey in the jar.'")
+	AddAction(ircproj, `#whiskey`,  "takes a swig of Jameson, hands the bottle to %v, and sings - \"Whack fol de daddy-o, There's whiskey in the jar.\"")
 	AddAction(ircproj, `#shiraz`,  "wonders if %v has ever had a Heathcote Estate Shiraz?")
 
 	ircproj.Loop()
